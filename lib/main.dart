139f14850e10bafcd3696bd83a1d3aeb3cbf6d3a
Bryan dos Santos Gomes <bryandossantosg8@gmail.com>
2021-06-21 08:48:30 -0300

    My first application using flutter and material

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.purple),
-    home: Scaffold(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: HomePage()
+      )
+    );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Meu Primeiro App!"),
+        title: Text("Contador"),
       ),
-      body: Container(
-      color: Colors.purple,
-      child: Center(
-        child: Container(
-          color: Colors.green,
-          height: 200,
-          width: 200,
-          ),
+      body: Center(
+          child: Text(
+        "CONTADOR\n $count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
       ),
-      ) 
-    ,)
-    )
-  );
+    );
+  }
 }
